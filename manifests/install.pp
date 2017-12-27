19c6484fbb48cd34de385a11952c6e5ea49a4dfd
Andreas Ziethen <az@ziethen.net>
2017-12-27 18:40:38 +0100

    add gitlab script for repo registration

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,11 +3,15 @@
 class gitlab_ci_multi_runner::install {
 
   if( $gitlab_ci_multi_runner::manage_package_repo ) {
-    class { 'gitlab_ci_multi_runner::repo': }
+    exec { "Register-GitLab-Repo":
+        command  => "curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash",
+        user     => root,
+    }
   }
 
   package { $gitlab_ci_multi_runner::package_name:
     ensure => present,
     tag    => 'gitlab-ci-multi-runner',
+    require => Exec['Register-GitLab-Repo'],
   }
 }
